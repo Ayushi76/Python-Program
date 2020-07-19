93deb87baf461c83d2a2788a219af533881e7a2a
Ayushi gaurav <53953293+Ayushi76@users.noreply.github.com>
2020-07-19 21:01:32 +0530

    program

---
+++ greatest.py
@@ -0,0 +1,10 @@
+first=int(input("enter a first number"))
+print("first")
+second=int(input("enter a second number"))
+print("second")
+if first>second:
+    print("greatest is",first)
+elif second>first:
+    print("greatest is",second)
+else:
+    print("both are equal")
