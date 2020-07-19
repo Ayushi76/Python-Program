0fe6463ef12242f5285ee473fb30694b8f757d9a
Ayushi gaurav <53953293+Ayushi76@users.noreply.github.com>
2020-07-19 20:59:05 +0530

    program

---
+++ square_or_not.py
@@ -0,0 +1,8 @@
+length=int(input("enter the lenght"))
+print("length")
+breadth=int(input("enter the breadth"))
+print("breadth")
+if length == breadth:
+    print("yes, it is square")
+else:
+    print("No, it is only Rectangle")
